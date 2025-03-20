c3f64b66e9ae6fe3d2f91e06b9b032085d68025f
minhaj-xo <minhaj.mj.2024@gmail.com>
2025-03-20 23:13:47 +0600

    x

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./apps/**/templates/**/*.html",
+      "./templates/**/*.html",
+      "./static/js/**/*.js",
+      "./**/templates/**/*.html",
+      "./**/static/**/*.js",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
