58e7f4b96ee9740c81f0c1cc47950946e642d4a5
youngdumbbrokeshajib <md.shajib.hossain@g.bracu.ac.bd>
2025-04-10 01:55:22 +0600

    raw mat edit,admin site edit

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import ProductTemplate,ProductCategory,Packaging,ProductVariant,BillOfMaterials
 # Register your models here.
+
+admin.site.register(ProductVariant)
+admin.site.register(ProductCategory)
+admin.site.register(ProductTemplate)
+admin.site.register(BillOfMaterials)
